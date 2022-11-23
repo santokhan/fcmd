cdb66e7071e82619b842f4a1580f00e188f8627d
Santo Khan <santokhan1999@gmail.com>
2022-11-23 19:38:56 +0600

    Countdown timer

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,8 +11,44 @@ const App = {
         }
       });
   },
+  countDownTimer() {
+    // Set the date we're counting down to
+    var countDownDate = new Date("Dec 9, 2022 15:37:25").getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function () {
+      // Get today's date and time
+      var now = new Date().getTime();
+
+      // Find the distance between now and the count down date
+      var distance = countDownDate - now;
+
+      // Time calculations for days, hours, minutes and seconds
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      // Display the result in the element with id="demo"
+      // document.getElementById("demo").innerHTML =
+      //   days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+      document.querySelector("header #days").innerHTML = days;
+      document.querySelector("header #hours").innerHTML = hours;
+      document.querySelector("header #minutes").innerHTML = minutes;
+      document.querySelector("header #seconds").innerHTML = seconds;
+
+      // If the count down is finished, write some text
+      if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+      }
+    }, 1000);
+  },
 };
 
 window.onload = () => {
   App.showHideDropdown();
+  App.countDownTimer();
 };
