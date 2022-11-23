ea9566bfa114c6cce20cff03c9ade5786b88fda6
Santo Khan <santokhan1999@gmail.com>
2022-11-23 22:38:43 +0600

    More responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,9 @@
 const App = {
   showHideDropdown() {
+    const target = document.getElementById("dropdownNav");
     document
       .getElementById("dropdownToggler")
       .addEventListener("click", (e) => {
-        const target = document.getElementById("dropdownNav");
         if (target.style.display === "none") {
           target.style.display = "block";
         } else {
